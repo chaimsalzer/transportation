2c40e2d28e5d7ed10a92ae52ea88312acbc63bb2
Chaim Salzer <chaim.salzer@diamonds.net>
2019-05-07 18:42:44 +0300

    connect to firestore

--- functions/index.js
+++ functions/index.js
@@ -1,8 +1,30 @@
 const functions = require("firebase-functions");
+const trans = require("./transportationRequest");
+const admin = require("firebase-admin");
+admin.initializeApp(functions.config().firebase);
+
+var db = admin.firestore();
 
 // Create and Deploy Your First Cloud Functions
 // https://firebase.google.com/docs/functions/write-firebase-functions
 
-exports.helloWorld = functions.https.onRequest((request, response) => {
-  response.send("Hello from Firebase!");
+exports.helloWorld = functions.https.onRequest(async (_, res) => {
+  // const transportationData = trans.getStationData();
+
+  const docRef = await db.collection("stations").doc("21258");
+
+  const stationSet = docRef.set({
+    lines: {
+      400: { x: 1, y: 2 }
+    }
+  });
+  res.redirect(303, stationSet.toString());
+
+  // res.redirect(303, stationSet.toString());
 });
+
+// exports.accountcleanup = functions.pubsub
+//   .schedule("every 1 minute")
+//   .onRun(async context => {
+//     console.log("User cleanup finished");
+//   });
--- functions/transportationRequest.js
+++ functions/transportationRequest.js
@@ -1,13 +1,6 @@
-const functions = require("firebase-functions");
 const constants = require("./constants");
 const axios = require("axios");
 var convert = require("xml-js");
-const admin = require("firebase-admin");
-const cors = require('cors')({
-  origin: true,
-});
-
-admin.initializeApp();
 
 const { requestBody, url, headers } = constants;
 const requestOptions = { compact: true, ignoreComment: true, spaces: 4 };
@@ -24,11 +17,9 @@ request["siri:StopMonitoringRequest"]["siri:StartTime"] = nowIso;
 request["siri:StopMonitoringRequest"]["siri:MonitoringRef"] = 21258;
 request["siri:StopMonitoringRequest"]["siri:PreviewInterval"] = "PT12H";
 
-console.log(request);
-
 const data = convert.js2xml(requestBody, requestOptions);
 
-const transRequestAsync = async () => {
+const getStationData = async () => {
   const responseXml = await axios({ method: "post", url, headers, data });
   const responseObject = convert.xml2js(responseXml.data, {
     compact: true,
@@ -67,50 +58,7 @@ const transRequestAsync = async () => {
       }
     });
   });
-  console.log(stationVisits);
+  return stationVisits;
 };
 
-// transRequestAsync();
-
-// export const scheduledFunction = functions.pubsub
-//   .schedule("every 1 minutes")
-//   .onRun(context => {
-//     console.log("This will be run every 5 minutes!");
-//   });
-
-//   export scheduledFunction = functions.pubsub.schedule(‘every 5 minutes’).onRun((context) => {
-//     console.log(‘This will be run every 5 minutes!’);
-//   });
-
-
-
-exports.date = functions.https.onRequest((req, res) => {
-  // [END trigger]
-  // [START sendError]
-  // Forbidding PUT requests.
-  if (req.method === 'PUT') {
-    return res.status(403).send('Forbidden!');
-  }
-  // [END sendError]
-
-  // [START usingMiddleware]
-  // Enable CORS using the `cors` express middleware.
-  return cors(req, res, () => {
-    // [END usingMiddleware]
-    // Reading date format from URL query parameter.
-    // [START readQueryParam]
-    let format = req.query.format;
-    // [END readQueryParam]
-    // Reading date format from request body query parameter
-    if (!format) {
-      // [START readBodyParam]
-      format = req.body.format;
-      // [END readBodyParam]
-    }
-    // [START sendResponse]
-    const formattedDate = 'fatter';
-    console.log('Sending Formatted date:', formattedDate);
-    res.status(200).send(formattedDate);
-    // [END sendResponse]
-  });
-});
+module.exports.getStationData = getStationData;  
